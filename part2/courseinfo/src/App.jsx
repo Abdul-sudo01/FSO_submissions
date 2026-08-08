ce212aa7dca785f1efa3a442a9d9b68a72ef4257
Abdul Rehman <rehmanikhan471@gmail.com>
2026-08-08 14:04:53 +0500

    Sum of Exercises added(Ex 2.2)

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,13 +1,20 @@
 const Header = ({ heading }) => <h1>{heading}</h1>;
-const Content = ({ content }) => (
+const Content = ({ content, sum }) =>
+{
+const sumOfExercises = sum.reduce((sum, currentExercise) => 
+     sum + currentExercise.exercises  , 0 )
+ 
+  return (
   <div>
     {content.map((eachPart) => (
       <p key={eachPart.id}>
         {eachPart.name} {eachPart.exercises}
       </p>
     ))}
+    <p><strong> total of {sumOfExercises} exercises </strong></p>
   </div>
-);
+ )}; 
+
 const App = () => {
   const course = {
     id: 1,
@@ -28,12 +35,17 @@ const App = () => {
         exercises: 14,
         id: 3,
       },
+      {
+        name: "redux",
+        exercises: 11,
+        id: 4,
+      },
     ],
   };
   return (
     <div>
       <Header heading={course.name} />
-      <Content content={course.parts} />
+      <Content content={course.parts} sum={course.parts} />
     </div>
   );
 };

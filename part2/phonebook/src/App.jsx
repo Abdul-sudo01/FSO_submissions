56005c24c9905d8c4a78e368f8e7c50b39658b0f
Abdul Rehman <rehmanikhan471@gmail.com>
2026-08-10 17:06:28 +0500

    New Names Saved & rendered on screen(ex2.6)

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  console.log(newName);
+
+  const addName = (event) => {
+    event.preventDefault();
+    // console.log(addName.target.value);
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+        <div>debug : {newName} </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+         {persons.map((each) =>
+         <li key={each.name} > {(each.name)} </li>
+          )} 
+      </ul>
+    </div>
+  );
+};
+
+export default App;

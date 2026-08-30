449aabfe1b7a9bf98bd494742369a7482cbd0b79
Abdul Rehman <rehmanikhan471@gmail.com>
2026-08-30 09:10:30 +0500

    Notification functionality added(EX. 2.16)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,72 @@
 import { useEffect, useState } from "react";
-import PersonForm  from "./components/PersonForm";
-import DisplayList  from "./components/DisplayList";
-import Filter  from "./components/Filter";
-import personService  from "./services/persons";
+import PersonForm from "./components/PersonForm";
+import DisplayList from "./components/DisplayList";
+import Filter from "./components/Filter";
+import personService from "./services/persons";
+import Notification from "./components//Notification";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [filter, setFilter] = useState("");
- 
+  const [errorMessage, setErrorMessage] = useState(null);
+
   useEffect(() => {
     personService.getAll().then((initialPersons) => setPersons(initialPersons));
   }, []);
 
-  const savePerson = ( personObject ) =>
-    personService.create(personObject).then((returnedPerson) =>
-      setPersons(persons.concat(returnedPerson))
-    );
-const deletePerson = (id) => personService.deletePerson(id).then(() => setPersons(persons.filter(p => p.id !== id)))
+  const savePerson = (personObject) => {
+    personService
+      .create(personObject)
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setErrorMessage(`${returnedPerson.name} successfully added `);
+        setTimeout(() => setErrorMessage(null), 5000);
+      })
+      .catch((e) => {
+        setErrorMessage(`${e.response.data}`);
+        setTimeout(() => setErrorMessage(null), 5000);
+      });
+  };
 
-const replacePerson = ({id , number , name }) => {
-  const updatedPerson = {name , number}  
+  const deletePerson = (id) => 
+    personService.deletePerson(id).then(() => {
+      setPersons(persons.filter((p) => p.id !== id));
+    
+    });
 
-  personService.update({id , updatedPerson}).then(returnedPerson => setPersons(persons.map(per=>per.id !== id ? per : returnedPerson)))
-}
+  const replacePerson = ({ id, number, name }) => {
+    const updatedPerson = { name, number };
+    personService
+      .update({ id, updatedPerson })
+      .then((returnedPerson) => {
+        setPersons(
+          persons.map((per) => (per.id !== id ? per : returnedPerson)),
+        );
+        setErrorMessage(`${returnedPerson.name} successfuly replaced`);
+        setTimeout(() => setErrorMessage(null), 5000);
+      })
+      .catch((e) => {
+        setErrorMessage(`${e.response.data}`);
+        setTimeout(() => setErrorMessage(null), 5000);
+      });
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter filter={filter} setFilter={setFilter} />
       <h2>add a new </h2>
-      <PersonForm persons={persons} savePerson={savePerson} replacePerson={replacePerson}  />
+      <PersonForm
+        persons={persons}
+        savePerson={savePerson}
+        replacePerson={replacePerson}
+      />
       <h2>Numbers</h2>
-      <DisplayList persons={persons} filter={filter} deletePerson = {deletePerson} />
+      <DisplayList
+        persons={persons}
+        filter={filter}
+        deletePerson={deletePerson}
+      />
     </div>
   );
 };

754f58e8b1101524fbcbf1c93bf174a54646bb85
Abdul Rehman <rehmanikhan471@gmail.com>
2026-09-07 15:27:17 +0500

    MongoDB: Deleting pre-saved entries from database(EX 3.15)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,9 +4,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 morgan.token("PORT", (req, res) => `Server running on port: ${PORT}`);
 app.use(morgan(":PORT"));
@@ -32,14 +32,6 @@ app.get("/persons", (req, res) => {
     res.json(p);
   });
 });
-
-// app.get("/info", (req, res) => {
-//   res.send(
-//     ` <div> Phonebook has info for  ${persons.length}  people </div> <br /> 
-//     <div> ${new Date()} </div>`,
-//   );
-// });
-
 app.get("/persons/:id", (req, res) => {
   const id = req.params.id;
   const person = persons.find((p) => p.id === id);
@@ -49,28 +41,41 @@ app.get("/persons/:id", (req, res) => {
     res.status(404).end();
   }
 });
-app.delete("/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+app.delete("/persons/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const process = await Persons.findByIdAndDelete(id);
+    console.log(process);
+    res.status(204);
+  } catch (error) {
+    next(error);
+  }
 });
 app.post("/persons", async (req, res) => {
   const body = req.body;
-  
-  if (!body.name) return res.status(400).json({ error: "provide name to proceed!" });
-  if (!body.number) return res.status(400).json({ error: "provide Number to proceed!" });
-  
+
+  if (!body.name)
+    return res.status(400).json({ error: "provide name to proceed!" });
+  if (!body.number)
+    return res.status(400).json({ error: "provide Number to proceed!" });
+
   const name = body.name;
   const number = body.number;
-  const person = new Persons({ name , number })
+  const person = new Persons({ name, number });
 
-  const nameCheck = await Persons.findOne({ name })
+  const nameCheck = await Persons.findOne({ name });
   console.log("found person :", nameCheck);
-  if (nameCheck) return res.status(400).json({error : 'name must be unique'})  
-  
-res.status(201).json( await person.save());
+  if (nameCheck) return res.status(400).json({ error: "name must be unique" });
+
+  res.status(201).json(await person.save());
 });
 
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+  if (error.name === "CastError")
+    return res.status(404).json({ error: "incorrect format!" });
+};
+app.use(errorHandler);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on port : ${PORT}`);

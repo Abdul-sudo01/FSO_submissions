f13cd7432e118d9e96d62dea0b911a6641fab36c
Abdul Rehman <rehmanikhan471@gmail.com>
2026-09-08 17:03:49 +0500

    ESLINT : Lint configuration(EX 3.22)

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,99 +1,90 @@
-require("dotenv").config();
-const Persons = require("./models/persons");
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const app = express();
-const cors = require("cors");
-app.use(express.json());
-app.use(cors());
+require('dotenv').config()
+const Persons = require('./models/persons')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+app.use(express.json())
+app.use(cors())
 
-morgan.token("PORT", (req, res) => `Server running on port: ${PORT}`);
-app.use(morgan(":PORT"));
+morgan.token('PORT', () => `Server running on port: ${PORT}`)
+app.use(morgan(':PORT'))
 
-morgan.token("postReq", (req, res) => JSON.stringify(req.body));
+morgan.token('postReq', (req) => JSON.stringify(req.body))
 app.use(
   morgan(
-    ":method :url :status :res[response-length] :response-time ms  :postReq",
+    ':method :url :status :res[response-length] :response-time ms  :postReq',
   ),
-);
+)
 
-mongoose.set("toJSON", {
-  transform: (document, receivedObject) => {
-    receivedObject.id = receivedObject._id.toString();
-    delete receivedObject._id;
-    delete receivedObject.__v;
-  },
-});
-
-app.get("/persons", (req, res) => {
+app.get('/persons', (req, res) => {
   Persons.find({}).then((p) => {
-    console.log(p);
-    res.json(p);
-  });
-});
-app.get("/persons/:id", async (req, res , next) => {
+    console.log(p)
+    res.json(p)
+  })
+})
+app.get('/persons/:id', async (req, res , next) => {
   try {
-  const id = req.params.id;
-  const person =  await Persons.findById(id)
-  console.log(person)
-   if (!person) return res.status(404).end()
-  res.json(person);
+    const id = req.params.id
+    const person =  await Persons.findById(id)
+    console.log(person)
+    if (!person) return res.status(404).end()
+    res.json(person)
   } catch (error) {next(error)}
-});
-app.delete("/persons/:id", async (req, res, next) => {
+})
+app.delete('/persons/:id', async (req, res, next) => {
   try {
-    const id = req.params.id;
-    const process = await Persons.findByIdAndDelete(id);
-    console.log(process);
-    res.status(204);
+    const id = req.params.id
+    const process = await Persons.findByIdAndDelete(id)
+    console.log(process)
+    res.status(204)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
-app.put("/persons/:id", async (req, res, next) => {
+})
+app.put('/persons/:id', async (req, res, next) => {
   try {
-    const { name, number } = req.body;
-    const id = req.params.id;
+    const { name, number } = req.body
+    const id = req.params.id
     const update = await Persons.findByIdAndUpdate(
       id,
       { name, number },
       { new: true, runValidators: true },
-    );
-    console.log("update :-", update);
-    if (!update) return res.status(404).end();
-    res.json(update);
+    )
+    console.log('update :-', update)
+    if (!update) return res.status(404).end()
+    res.json(update)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
-app.post("/persons", async (req, res) => {
-  const body = req.body;
+})
+app.post('/persons', async (req, res) => {
+  const body = req.body
 
   if (!body.name)
-    return res.status(400).json({ error: "provide name to proceed!" });
+    return res.status(400).json({ error: 'provide name to proceed!' })
   if (!body.number)
-    return res.status(400).json({ error: "provide Number to proceed!" });
+    return res.status(400).json({ error: 'provide Number to proceed!' })
 
-  const name = body.name;
-  const number = body.number;
+  const name = body.name
+  const number = body.number
 
-  const person = new Persons({ name, number });
-  res.status(201).json(await person.save());
+  const person = new Persons({ name, number })
+  res.status(201).json(await person.save())
 
   // const nameCheck = await Persons.findOne({ name });
   // console.log("found person :", nameCheck);
   // if (nameCheck)
   //   return res.status(400).json({ error: "name must be unique" });
-});
+})
 
-const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
-  if (error.name === "CastError")
-    return res.status(404).json({ error: "incorrect format!" });
-};
-app.use(errorHandler);
-const PORT = process.env.PORT;
+const errorHandler = (error, req, res, ) => {
+  console.log(error.message)
+  if (error.name === 'CastError')
+    return res.status(404).json({ error: 'incorrect format!' })
+}
+app.use(errorHandler)
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`server running on port : ${PORT}`);
-});
+  console.log(`server running on port : ${PORT}`)
+})
--- part3/phonebook_backend/models/persons.js
+++ part3/phonebook_backend/models/persons.js
@@ -1,16 +1,25 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 mongoose
   .connect(process.env.MONGODB_URI, { family: 4 })
-  .then(() => console.log("connected to MongoDB"))
-  .catch(error => console.log("connection error!", error));
+  .then(() => console.log('connected to MongoDB'))
+  .catch(error => console.log('connection error!', error))
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
-const Persons = mongoose.model("Person", personSchema);
 
-module.exports = mongoose.model("Person", personSchema);
+
+mongoose.set('toJSON', {
+  transform: (document, receivedObject) => {
+    receivedObject.id = receivedObject._id.toString()
+    delete receivedObject._id
+    delete receivedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', personSchema)
+
